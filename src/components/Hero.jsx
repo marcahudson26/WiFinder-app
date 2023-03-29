e4772198d1e68b9db5930d2ec064c4b5c6e2f975
marc hudson <marchudson2601@gmail.com>
2023-03-29 22:46:39 +0100

    fixed pin relative location problem

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,13 @@ const Hero = () => {
       {/* hero image */}
       <div className={`${styles.heroImg} w-full -mt-4`}>
         <img width="680px" src={heroApp} alt="isometric city vector art" />
-        <div className={`${styles.heroPin}`}>
-          <img
-            className={`${styles.pin} animate-floating`}
-            src={pinIcon}
-            width="30px"
-            alt="search pin"
-          />
-        </div>
+        className={`${styles.heroPin}`}
+        <img
+          className={`${styles.pin} animate-floating`}
+          src={pinIcon}
+          width="30px"
+          alt="search pin"
+        />
       </div>
     </>
   );
